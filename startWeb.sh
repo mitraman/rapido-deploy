59c9a5f4ab8b4250095cce076c7c531f9da8e961
Ronnie Mitra <ronnie.mitra@gmail.com>
2017-08-08 23:31:38 -0400

    tls setup

--- startWeb.sh
+++ startWeb.sh
@@ -1,1 +1,1 @@
-docker run --network=isolated_nw --name=web -d -p 80:80 rapido-web:release
+docker run --network=isolated_nw --name=web -d -p 80:80 -v /etc/letsencrypt/live/rapidodesigner.com/:/certs:ro rapido-web:release
